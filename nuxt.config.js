4b1199653bfd8ac41c74c4fbf56f1407e20fcf3f
Ben Pilgrim <ben@pilgrim.me.uk>
2019-06-12 18:34:20 +0100

    Fix fonts and healthcheak

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,13 @@ module.exports = {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
